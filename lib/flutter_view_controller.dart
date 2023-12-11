db6204aa85c44d2d33369ef78c2f61550f603e90
Guilherme Tognetti <email.guilherme.t@gmail.com>
2023-12-11 20:56:22 -0300

    [ISOLATED] ISOLATED PLUG FROM PROJECT

--- lib/flutter_view_controller.dart
+++ lib/flutter_view_controller.dart
@@ -2,7 +2,7 @@ library flutter_view_controller;
 
 export 'src/global_state.dart';
 export 'src/notifier.dart';
-export 'src/plug.dart';
+export 'package:plug/plug.dart';
 export 'src/view.dart';
 export 'src/screen_size.dart';
 export 'src/navigator_monitor.dart';
