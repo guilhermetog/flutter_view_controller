3fc52b50a25421eb47d2eee1b9bde766be97b8fb
Guilherme Tognetti <email.guilherme.t@gmail.com>
2024-05-20 14:35:11 -0300

    ammend

--- lib/src/global_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_view_controller/src/notifier.dart';
-
-class GlobalState<T> {
-  static final Map<Type, Notifier> _states = {};
-  T get current => GlobalState._states[T]!.value;
-  set current(T value) => GlobalState._states[T]!.value = value;
-
-  register(T value) => GlobalState._states[T] = Notifier(value);
-}
