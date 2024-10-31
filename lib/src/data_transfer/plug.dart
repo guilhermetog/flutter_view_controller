458acdcc3e86028acd417ba221d703438cede647
Guilherme Tognetti <email.guilherme.t@gmail.com>
2024-10-31 11:12:20 -0300

    make plug isconnected public

--- lib/src/data_transfer/plug.dart
+++ lib/src/data_transfer/plug.dart
@@ -1,24 +1,24 @@
 class Plug<T> {
   Function callback = () async {};
-  bool _isConnected = false;
+  bool isConnected = false;
 
   then(Function() f) {
-    _isConnected = true;
+    isConnected = true;
     callback = f;
   }
 
   take(Function(T) f) {
-    _isConnected = true;
+    isConnected = true;
     callback = f;
   }
 
   call() async {
-    if (!_isConnected) return;
+    if (!isConnected) return;
     await callback();
   }
 
   send(T arg) async {
-    if (!_isConnected) return;
+    if (!isConnected) return;
     await callback(arg);
   }
 }
